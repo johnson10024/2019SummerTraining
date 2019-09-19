adeed3e79712baf0eb850940bb444805f581ec67
jonhnson10024 <johnson.6106@hotmail.com.tw>
2019-09-19 10:57:36 +0800

    Add templates

---
+++ template_n.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <map>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define INF (0x3f3f3f3f)
+#define MOD (998244353)
+#define PREC (0.0000001)
+#define X first
+#define Y second
+// #define DEBUG
+
+using namespace std;
+
+typedef long long int ll;
+typedef pair<int, int> pii;
+
+void init()
+{
+
+}
+
+void solve()
+{
+
+}
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+	while(n--)
+	{
+		init();
+		solve();
+	}
+	
+	return 0;
+}
